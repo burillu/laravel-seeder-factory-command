fd10a69986c6aa44f35b1c25b96ef38c7b356f64
burillu <burillu90@hotmail.it>
2024-03-21 18:54:04 +0100

    updating featured with CLI ok

---
+++ app/Console/Commands/UpdateProducts.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Models\Product;
+
+class UpdateProducts extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'products:update-featured';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Update Featured Products changing price and related category';
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        //prendo i prodotti sponsorizzati
+        $featured = Product::where('sponsored',1)->get();
+        //dd($featured);
+        $newprice = 1 - 0.01;
+        foreach($featured as $key => $product){
+            $newprice +=10;
+            $product->price = $newprice ;
+            $product->category_id = $key+1;
+            $product->save();
+        };
+         dd($featured);
+
+        return Command::SUCCESS;
+    }
+}
